d2b9883a8ef92929a386f32dccca929ce667c3ac
BarathNallusamy <nallu.baray@gmail.com>
2021-05-05 13:46:07 +0100

    Initial commit with tests generated via specflow

--- BDD_Calculator/BDD_Calculator_Test/CalculatorFeature.feature
+++ BDD_Calculator/BDD_Calculator_Test/CalculatorFeature.feature
@@ -1,9 +1,62 @@
 ﻿Feature: CalculatorFeature
 	Simple calculator for adding two numbers
+	in order to avoid silly mistakes
+	As a math idiot
+	I want to be able to do simple calculations
 
 @mytag
 Scenario: Add two numbers
-	Given the first number is 50
-	And the second number is 70
-	When the two numbers are added
-	Then the result should be 120
+	Given I have a calculator
+	And I enter 5 and 2 into the calculator
+	When I press add
+	Then the result should be 7
+
+@mytag
+Scenario: Subtract two numbers
+	Given I have a calculator
+	And I enter <input1> and <input2> into the calculator
+	When I press subtract
+	Then the result should be <result>
+
+Examples:
+| input1 | input2 | result | 
+| 1      | 1      | 0      | 
+| 0      | 1      | -1     | 
+| 100    | -1     | 101    |
+
+@mytag
+Scenario: Multiply
+	Given I have a calculator
+	And I enter <input1> and <input2> into the calculator
+	When I press multiply
+	Then the result should be <result>
+Examples:
+| input1 | input2 | result | 
+| 1      | 1      | 1      | 
+| 2      | 3	  | 6      | 
+| 9      | 9      | 81     |
+| 5      | -17    | -85    |
+
+
+@mytag
+Scenario: Divide
+	Given I have a calculator
+	And I enter <input1> and <input2> into the calculator
+	When I press divide
+	Then the result should be <result>
+Examples:
+| input1 | input2 | result | 
+| 1      | 1      | 1      | 
+| 8      | 2	  | 4      | 
+| 100    | 4      | 25     |
+
+
+@mytag
+Scenario: Divide by Zero
+	Given I have a calculator
+	And I enter <input1> and 0 into the calculator
+	Then a DivideByZero Exception should be thrown with the exception message "Cannot Divide By Zero" when I press Divide
+Examples:
+| input1 | 
+| 1      | 
+| 6      | 
--- BDD_Calculator/BDD_Calculator_Test/CalculatorFeatureSteps.cs
+++ BDD_Calculator/BDD_Calculator_Test/CalculatorFeatureSteps.cs
@@ -1,33 +1,65 @@
 ﻿using System;
 using TechTalk.SpecFlow;
+using BDD_Calculator;
+using NUnit.Framework;
 
 namespace BDD_Calculator_Test
 {
     [Binding]
     public class CalculatorFeatureSteps
     {
+        private Calculator _calculator;
+        private int _results;
+
         [Given(@"I have a calculator")]
         public void GivenIHaveACalculator()
         {
-            ScenarioContext.Current.Pending();
+            _calculator = new Calculator();
         }
         
         [Given(@"I enter (.*) and (.*) into the calculator")]
-        public void GivenIEnterAndIntoTheCalculator(int p0, int p1)
+        public void GivenIEnterAndIntoTheCalculator(int num1, int num2)
         {
-            ScenarioContext.Current.Pending();
+            _calculator.Num1 = num1;
+            _calculator.Num2 = num2;
         }
         
         [When(@"I press add")]
         public void WhenIPressAdd()
         {
-            ScenarioContext.Current.Pending();
+            _results = _calculator.Add();
         }
-        
-        [Then(@"the result should be (.*)")]
-        public void ThenTheResultShouldBe(int p0)
+
+        [When(@"I press subtract")]
+        public void WhenIPressSubtract()
         {
-            ScenarioContext.Current.Pending();
+            _results = _calculator.Subtract();
+        }
+
+        [When(@"I press multiply")]
+        public void WhenIPressMultiply()
+        {
+            _results = _calculator.Multiply();
+        }
+
+        [When(@"I press divide")]
+        public void WhenIPressDivide()
+        {
+            _results = _calculator.Divide();
+        }
+
+        [Then(@"a DivideByZero Exception should be thrown with the exception message ""(.*)"" when I press Divide")]
+        public void ThenADivideByZeroExceptionShouldBeThrownWithTheExceptionMessageWhenIPressDivide(string expectedResults)
+        {
+            Assert.That(() => _calculator.Divide(), Throws.Exception.With.Message.EqualTo(expectedResults));
+        }
+
+
+
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int expectedResults)
+        {
+            Assert.That(_results, Is.EqualTo(expectedResults));
         }
     }
 }
